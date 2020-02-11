5795f98e987b90786a55fb2e5b6a5b01a24d0ee3
Ben Hickson <bzhickson@gmail.com>
2020-02-11 03:34:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
 # My Code here....
+def map_to_negativize(array)
+  output = []
+  array.length.times do |i|
+    output.push(array[i] * -1)
+  end
+  output
+end
+
+def map_to_no_change(array)
+  array
+end
+
+def map_to_double(array)
+  output = []
+  array.length.times do |i|
+    output.push(array[i] * 2)
+  end
+  output
+end
+
+def map_to_square(array)
+  output = []
+  array.length.times do |i|
+    output.push(array[i] ** 2)
+  end
+  output
+end
+
+def reduce_to_total(array, starting_point = 0)
+  output = starting_point
+  array.length.times do |i|
+    output = output + array[i])
+  end
+   
+  output
+end
